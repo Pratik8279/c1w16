9ad4638dc6572a518fe65b3843bbdc283459380a
Pratik <paulpratik6@gmail.com>
2022-04-11 12:54:05 +0530

    couldnt get the data

--- src/components/GroceryItem.jsx
+++ src/components/GroceryItem.jsx
@@ -1,5 +1,15 @@
 // reusable card component
-const GroceryItem = () => {
-  return <></>;
+const GroceryItem = ({mrp,id,discount,imgURL,sellingPrice,title}) => {
+ 
+  return <>
+      <img src= {imgURL} alt="" />
+      <p>{title}</p>
+      <p>{mrp}</p>
+      <p>{discount}</p>
+      <p>{sellingPrice}</p>
+      <p>{id}</p>
+     
+  
+  </>;
 };
 export default GroceryItem;
--- src/components/Grocerydetails.jsx
+++ src/components/Grocerydetails.jsx
@@ -1,17 +1,41 @@
 import React from "react"
 import data from '../data.json'
+import GroceryItem from "./GroceryItem"
+import { useState } from "react"
+
+
+
 
 
 const GroceryDetails = ()=>{
-console.log(data)
 
+   console.log(data)
+
+
+
+
+
+// "mrp": "₹ 185.00",
+// "sellingPrice": "₹ 92.00",
+// "discount": "50%"
     return(
+        
         <>
+  
         <h1>Groceries</h1>
         <div className="container">
             {/* map through the data and display the cards */}
+            {data.map((item)=>{
+        
+             <GroceryItem {...item}/>
+          
+            })
+}
+            
         </div>
         </>
     )
 }
+
+
 export default GroceryDetails
